77a22a72553af24db87bb2ac6f381fa9f6c93ba8
Aashu <mt2332678@gmail.com>
2024-02-27 13:58:32 +0530

    second commmit all

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+
 
 
 const Home = () => {
-  const navigate = useNavigate();
+ 
 
   const addCart = () => {
     
